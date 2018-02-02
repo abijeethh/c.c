1410b1e9f8819d0f9c388bb58940fb303140a908
abijeethh <35689017+abijeethh@users.noreply.github.com>
2018-02-02 16:48:33 +0530

    Create program4.c

---
+++ program4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+  char a = 'H', b = 'e', c = 'l', d = 'o';
+  char e = 'w', f = 'r', g = 'd';
+ 
+  printf("%c%c%c%c%c %c%c%c%c%c", a, b, c, c, d, e, d, f, c, g);
+  getch();
+ 
+  return 0;
+}
